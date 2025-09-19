c320e0b1f3334771f25c48018a349ffe11027afa
Rituparno Das <95102828+Almost-Developer@users.noreply.github.com>
2025-09-19 16:53:35 +0530

    First Chapter

--- Bean/src/main/java/First_Chapter/Bean/Entity/Person.java
+++ Bean/src/main/java/First_Chapter/Bean/Entity/Person.java
@@ -4,9 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
 public class Person {
     private String name;
     private int age;
