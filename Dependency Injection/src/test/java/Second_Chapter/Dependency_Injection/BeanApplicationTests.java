f6b58038b1e805f38bdae1ea7c20265d4b2922e4
Rituparno Das <95102828+Almost-Developer@users.noreply.github.com>
2025-10-24 12:12:27 +0530

    Second Chapter

---
+++ Dependency Injection/src/test/java/Second_Chapter/Dependency_Injection/BeanApplicationTests.java
@@ -0,0 +1,13 @@
+package Second_Chapter.Dependency_Injection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
