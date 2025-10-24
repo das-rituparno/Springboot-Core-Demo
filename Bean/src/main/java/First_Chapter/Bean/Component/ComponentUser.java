a2a021a1b39f6509db142df94539435c026e9b7e
Rituparno Das <95102828+Almost-Developer@users.noreply.github.com>
2025-10-24 12:11:42 +0530

    First Chapter

---
+++ Bean/src/main/java/First_Chapter/Bean/Component/ComponentUser.java
@@ -0,0 +1,10 @@
+package First_Chapter.Bean.Component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentUser {
+    public String createComponent() {
+        return "Hello from Component";
+    }
+}
