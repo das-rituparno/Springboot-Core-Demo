2a3564d0752558d57ae1683cb121dee2feea0fbf
das-rituparno <dasrituparno@gmail.com>
2025-11-30 14:11:08 +0530

    Thirteen Chapter

---
+++ Conditional on Property/src/test/java/com/conceptandcoding/conditionalonproperty/ConditionalonpropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conceptandcoding.conditionalonproperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConditionalonpropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
